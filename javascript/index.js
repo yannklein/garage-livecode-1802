781fdc9221b5d596dd953ab4d2dfb301338c47e4
Yann Klein <yann.klein@me.com>
2024-06-13 22:20:22 +0900

    livecode done

--- javascript/index.js
+++ javascript/index.js
@@ -1,75 +1,80 @@
 // DON'T CHANGE THIS LINE
-window.myBadAssGarage = 'pablos-cave';
-if (myBadAssGarage)
-  document.querySelector('#garage-name').innerText = myBadAssGarage.replace(
-    /-/g,
-    ' ',
-  );
+window.myBadAssGarage = 'ga-raj';
+if (myBadAssGarage) document.querySelector('#garage-name').innerText = myBadAssGarage.replace( /-/g, ' ', );
 // //////////////////////
 
-const url = `https://wagon-garage-api.herokuapp.com/${myBadAssGarage}/cars`;
+const url = `https://garage.api.lewagon.com/${myBadAssGarage}/cars`;
 
-// Get all the cars
+// //////
+// Get all cars
+// //////
+
 const getCars = () => {
-  // 1. Select elements (cars-list)
-  const carsList = document.querySelector('.cars-list');
-  // 2. No event to listen
-  // 3. Fetch the Wagon Garage, get the cars
+  // 1. Select cars-list
+  const carsList = document.querySelector(".cars-list");
+
+  // 2. No event listener needed
+
+  // 2.5 Fetch Garage API (GET)
   fetch(url)
-    .then((response) => response.json())
+    .then(response => response.json())
     .then((data) => {
       console.log(data);
-      // 4. Change the DOM (display on the cars-list)
-      carsList.innerHTML = '';
-      data.forEach((carInfo) => {
-        carsList.insertAdjacentHTML(
-          'afterbegin',
-          `<div class="car">
-        <div class="car-image">
-          <img src="http://loremflickr.com/280/280/${carInfo.brand},${carInfo.model}" />
-        </div>
-        <div class="car-info">
-          <h4>${carInfo.brand} ${carInfo.model}</h4>
-          <p><strong>Owner:</strong> ${carInfo.owner}</p>
-          <p><strong>Plate:</strong> ${carInfo.plate}</p>
-        </div>
-      </div>`,
-        );
+      carsList.innerHTML = "";
+      data.forEach((carData) => {
+        // 3. Change the DOM: create the car card (with API info) and insert it into cars-list
+        const carCard = `
+        <div class="car">
+          <div class="car-image">
+            <img src="http://loremflickr.com/280/280/${carData.brand},${carData.model}" />
+          </div>
+          <div class="car-info">
+            <h4>${carData.brand} ${carData.model}</h4>
+            <p><strong>Owner:</strong> ${carData.owner}</p>
+            <p><strong>Plate:</strong> ${carData.plate}</p>
+          </div>
+        </div>`;
+        carsList.insertAdjacentHTML("afterbegin", carCard);
       });
-    });
+    })
 };
 
 getCars();
 
-// 1. Select elements
-const brandInput = document.querySelector("#brand");
-const modelInput = document.querySelector("#model");
-const plateInput = document.querySelector("#plate");
-const ownerInput = document.querySelector("#owner");
-const addCarBtn = document.querySelector("#submit-btn");
-// 2. Listen to click on "add a car"
-addCarBtn.addEventListener("click", (event) => {
-  event.preventDefault();
+// //////
+// Add a car
+// //////
+
+// 1. Select form (new-car), 4 inputs
+const form = document.querySelector("#new-car");
+const brand = document.querySelector("#brand");
+const model = document.querySelector("#model");
+const plate = document.querySelector("#plate");
+const owner = document.querySelector("#owner");
+
+// 2. Listen to a submit on the form (event.preventDefault())
+form.addEventListener("submit", (event) => {
+  event.preventDefault()
   console.log(event);
-  // 3. Fetch API (POST)
-  const newCar = {
-    brand: brandInput.value,
-    model: modelInput.value,
-    owner: ownerInput.value,
-    plate: plateInput.value
-  };
+  // 2.5 Fetch the Garage API (POST)
+  const carData = {
+    brand: brand.value,
+    model: model.value,
+    owner: owner.value,
+    plate: plate.value
+  }
 
   const options = {
     method: "POST",
     headers: { "Content-Type": "application/json" },
-    body: JSON.stringify(newCar)
+    body: JSON.stringify(carData)
   };
 
   fetch(url, options)
-    .then(response => {response.json()})
+    .then(response => response.json())
     .then((data) => {
       console.log(data);
-      // 4. Change the DOM -> getCars();
+      // 3. Get all cars
       getCars();
     })
 });
